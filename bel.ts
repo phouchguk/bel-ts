0bfb75b2ed4108392fa88da2ffec8f3ce0c9d4fc
Phouchg <phouchg@protonmail.com>
2020-04-11 17:17:25 +0100

    start of evaluate

--- bel.ts
+++ bel.ts
@@ -1,5 +1,51 @@
-import { BelT } from "./type";
+import { BelT, Pair } from "./type";
+import { atom, car, cdr, pair } from "./pair";
+import { sym, symbol, t } from "./sym";
 import { Continuation } from "./continuation";
 import { Environment } from "./environment";
 
-export function evaluate(e: BelT, r: Environment, k: Continuation): void {}
+function number(x: BelT) {
+  return typeof x === "number";
+}
+
+function string(x: BelT) {
+  return typeof x === "string";
+}
+
+function taggedList(x: Pair, tag: symbol) {
+  return pair(x) && car(x) === tag;
+}
+
+const apply = sym("apply");
+const o = sym("o");
+const lit = sym("lit");
+
+function selfEvaluating(x: BelT) {
+  return (
+    x === null ||
+    number(x) ||
+    string(x) ||
+    taggedList(x as Pair, lit) ||
+    (symbol(x) && (x === t || x === o || x === apply))
+  );
+}
+
+function evaluateQuote(v: BelT, r: Environment, k: Continuation) {
+  k.resume(v);
+}
+
+function evaluateVariable(n: symbol, r: Environment, k: Continuation) {
+  r.lookup(n, k);
+}
+
+export function evaluate(e: BelT, r: Environment, k: Continuation): void {
+  if (atom(e)) {
+    if (selfEvaluating(e)) {
+      evaluateQuote(e, r, k);
+      return;
+    }
+
+    evaluateVariable(e as symbol, r, k);
+    return;
+  }
+}
--- main.ts
+++ main.ts
@@ -5,28 +5,36 @@ import { parse } from "./parse";
 import { print } from "./print";
 import { BaseCont, IfCont } from "./continuation";
 import { theEmptyEnvironment } from "./environment";
+import { evaluate } from "./bel";
 
 const p: Pair = join(1, join(2, join(3, null)));
 const test: symbol = sym("hello");
 
+const baseCont = new BaseCont(null, gotResult);
+
 function gotExp(exp: BelT): void {
+  evaluate(exp, theEmptyEnvironment, baseCont);
+}
+
+function gotResult(result: BelT): void {
   let output: string[] = [];
 
-  print(exp, output);
+  print(result, output);
 
   if (output.length > 0) {
     console.log("got expression:", output.join(""));
   }
 }
 
-console.log(
-  new IfCont(new BaseCont(null, gotExp), null, null, theEmptyEnvironment)
-);
-
+/*
+console.log(new IfCont(baseCont, null, null, theEmptyEnvironment));
 console.log(p);
 console.log(pair(p));
 console.log(car(p));
 console.log(cdr(p));
 console.log(symbol(test));
 console.log(nom(test));
-parse('(1 2 hello "groove town") `(3 2 nil (1 0) ,@a (9 ,z ,@(4 5)))', gotExp);
+*/
+
+parse("42", gotExp);
+//parse('(1 2 hello "groove town") `(3 2 nil (1 0) ,@a (9 ,z ,@(4 5)))', gotExp);
